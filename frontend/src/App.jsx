88dcb0c07befcad61e34afbd21f42afddbeae33b
Vandanx712 <159246115+Vandanx712@users.noreply.github.com>
2026-01-30 18:41:53 +0530

    feat: update frontend dependencies, remove unused CSS, and configure Tailwind CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <h1 className='text-red-500'>Hello</h1>
     </>
   )
 }
